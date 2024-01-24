dc8da8b12ef590b2456c2537776a97170f37352b
Simon LeMur <127620383+Lemur11@users.noreply.github.com>
2024-01-24 16:14:35 -0500

    New auton code after day 2 of testing

--- viqc_auton.py
+++ viqc_auton.py
@@ -1,49 +1,58 @@
-#config: {leftmotor:4, rightmotor(rev):9, gyro10:10, intake:[6(rev),12], hopper:[5, 11(rev)]}
+#config: {leftmotor:4, rightmotor(rev):9, brain_inertial:10, intake:[6(rev),12], hopper:[5, 11(rev)]}
 
-from vex import *
-MOMENTUM = 0
+MOMENTUM = 25
+ADJUSTMENT = 2;
 
 def move(dist, speed=80, kp=2):
   leftmotor.set_position(0, DEGREES)
   rightmotor.set_position(0, DEGREES)
-  gyro10.set_heading(0, DEGREES)
-  if dist > 0:
-      while leftmotor.position(DEGREES) < abs(dist):
-          error = (gyro10.rotation(DEGREES)) * kp
-          leftmotor.set_velocity(speed - error)
-          rightmotor.set_velocity(speed + error)
+  brain_inertial.set_rotation(0, DEGREES)
+  # speed *= -1
+  if dist < 0:
+      while leftmotor.position(DEGREES) > dist:
+          error = (brain_inertial.rotation(DEGREES)) * kp
+          print(leftmotor.position(DEGREES))
+          leftmotor.set_velocity(speed*-1 - (error+ADJUSTMENT))
+          rightmotor.set_velocity(speed*-1 + (error))
           leftmotor.spin(FORWARD)
           rightmotor.spin(FORWARD)
+          if (abs(brain_inertial.rotation()) < 20):
+              brain_inertial.set_rotation(0, DEGREES)
   else:
-      sped = speed*-1
-      while leftmotor.position(DEGREES) > abs(dist):
-          error = (gyro10.rotation()) * kp
-          leftmotor.set_velocity(sped + error)
-          rightmotor.set_velocity(sped - error)
+      while leftmotor.position(DEGREES) < dist:
+          print(leftmotor.position(DEGREES))
+          error = (brain_inertial.rotation()) * kp
+          leftmotor.set_velocity(speed + (error+ADJUSTMENT))
+          rightmotor.set_velocity(speed - (error))
           leftmotor.spin(FORWARD)
           rightmotor.spin(FORWARD)
+          if (abs(brain_inertial.rotation()) < 20):
+              brain_inertial.set_rotation(0, DEGREES)
   rightmotor.stop()
   leftmotor.stop()
 
 def gyro_turn(direction, momentum=MOMENTUM, speed=80):
-   gyro10.set_rotation(0, DEGREES)
-   if direction > gyro10.rotation():
-      while direction - momentum > gyro10.rotation():
+   gyro_10.calibrate(GyroCalibrationType.NORMAL)
+   gyro_10.set_rotation(0, DEGREES)
+   if direction > gyro_10.rotation():
+      while direction - momentum > -1*gyro_10.rotation():
+          print(gyro_10.rotation())
           leftmotor.set_velocity(speed, PERCENT)
           rightmotor.set_velocity(speed, PERCENT)
           leftmotor.spin(REVERSE)
           rightmotor.spin(FORWARD)
           wait(20, MSEC)
+      leftmotor.stop()
+      rightmotor.stop()
    else:
-      while direction - momentum < gyro10.rotation():
+      while direction + momentum < -1*gyro_10.rotation():
           leftmotor.set_velocity(speed, PERCENT)
           rightmotor.set_velocity(speed, PERCENT)
           leftmotor.spin(FORWARD)
           rightmotor.spin(REVERSE)
           wait(20, MSEC)
-   leftmotor.stop()
-   rightmotor.stop()
-
+      leftmotor.stop()
+      rightmotor.stop()
 intake.set_max_torque(100, PERCENT)
 hopper.set_max_torque(100, PERCENT)
 intake.set_velocity(100, PERCENT)
@@ -51,10 +60,49 @@ hopper.set_velocity(100, PERCENT)
 intake.spin(FORWARD)
 
 def run_path():
-  # path code goes here
-  move(300)
-  move(-300)
-  gyro_turn(90)
-  gyro_turn(-90)
+  move(601)
+# move(601)
+# gyro_turn(91)
+# move(296)
+# gyro_turn(-153)
+# move(331)
+
+# move(331)
+# gyro_turn(-27)
+# move(593)
+# gyro_turn(-27)
+# move(331)
+# gyro_turn(-63)
+# move(445)
+# gyro_turn(135)
+# move(421)
+# gyro_turn(-135)
+# move(445)
+# gyro_turn(-168)
+# move(756)
+# gyro_turn(25)
+# move(741)
+# gyro_turn(53)
+# move(593)
+# gyro_turn(180)
+# move(296)
+# gyro_turn(-90)
+# move(889)
+
+# move(296)
+# gyro_turn(180)
+# move(1186)
+# gyro_turn(-90)
+# move(741)
+# gyro_turn(-153)
+# move(663)
+# gyro_turn(10)
+# move(741)
+# gyro_turn(8)
+# move(629)
+# gyro_turn(-19)
+# move(343)
+# gyro_turn(-116)
+# move(593)
 
 run_path()
