f8a860963730bedef121c934fb5f81437cd957e3
mmnbvcxzlkjhgfd <107881038+mmnbvcxzlkjhgfd@users.noreply.github.com>
2024-01-26 10:29:50 -0500

    Update new_auton.py
    so i basically reset gyro after straits/turns.

--- new_auton.py
+++ new_auton.py
@@ -4,15 +4,15 @@ momentum = 5
 
 # Begin project code
 def forward(dist):
+    gyro_10.set_rotation(0, DEGREES)
     leftmotor.spin(FORWARD)
     rightmotor.spin(FORWARD)
     leftmotor.set_position(0, DEGREES)
     rightmotor.set_position(0, DEGREES)
     leftmotor.set_velocity(100, PERCENT)
     rightmotor.set_velocity(100, PERCENT)
-    # u sure u dont wanna reset gyro?
+    # u sure u dont wanna reset gyro? It was dot there so i just did it for u.
     while True:
-        #why is momentum in the move function
         if (rightmotor.position(DEGREES) + leftmotor.position(DEGREES))/2 - momentum >= dist:
             break
         rot = gyro_10.rotation()
@@ -25,6 +25,7 @@ def forward(dist):
 
 # motors always spin forward]
 def backward(dist):
+    gyro_10.set_rotation(0, DEGREES)
     leftmotor.spin(FORWARD)
     rightmotor.spin(FORWARD)
     leftmotor.set_position(0, DEGREES)
@@ -49,6 +50,7 @@ def move(dist):
         forward(dist)
 
 def turn_right(to_turn):
+    gyro_10.set_rotation(0, DEGREES)
     turn_momentum = 35
     leftmotor.spin(FORWARD)
     rightmotor.spin(FORWARD)
@@ -61,6 +63,7 @@ def turn_right(to_turn):
     rightmotor.stop()
 
 def turn_left(to_turn):
+    gyro_10.set_rotation(0, DEGREES)
     turn_momentum = 35
     leftmotor.spin(FORWARD)
     rightmotor.spin(FORWARD)
