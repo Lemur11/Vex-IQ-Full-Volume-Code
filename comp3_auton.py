8999e380931c3574e2b203511c208a81444f714a
mmnbvcxzlkjhgfd <107881038+mmnbvcxzlkjhgfd@users.noreply.github.com>
2024-02-04 13:06:29 -0500

    Made the code actually spin the wheels

--- comp3_auton.py
+++ comp3_auton.py
@@ -1,60 +1,3 @@
-# region VEXcode Generated Robot Configuration
-from vex import *
-import urandom
-
-# Brain should be defined by default
-brain = Brain()
-
-# Robot configuration code
-brain_inertial = Inertial()
-left_drive_smart = Motor(Ports.PORT4, 1.5, True)
-right_drive_smart = Motor(Ports.PORT11, 1.5, False)
-
-drivetrain = SmartDrive(left_drive_smart, right_drive_smart, brain_inertial, 200)
-
-
-# Make random actually random
-def setRandomSeedUsingAccel():
-    wait(100, MSEC)
-    xaxis = brain_inertial.acceleration(XAXIS) * 1000
-    yaxis = brain_inertial.acceleration(YAXIS) * 1000
-    zaxis = brain_inertial.acceleration(ZAXIS) * 1000
-    urandom.seed(int(xaxis + yaxis + zaxis))
-
-
-# Set random seed
-setRandomSeedUsingAccel()
-
-vexcode_initial_drivetrain_calibration_completed = False
-
-
-def calibrate_drivetrain():
-    # Calibrate the Drivetrain Inertial
-    global vexcode_initial_drivetrain_calibration_completed
-    sleep(200, MSEC)
-    brain.screen.print("Calibrating")
-    brain.screen.next_row()
-    brain.screen.print("Inertial")
-    brain_inertial.calibrate()
-    while brain_inertial.is_calibrating():
-        sleep(25, MSEC)
-    vexcode_initial_drivetrain_calibration_completed = True
-    brain.screen.clear_screen()
-    brain.screen.set_cursor(1, 1)
-
-
-# endregion VEXcode Generated Robot Configuration
-
-# ------------------------------------------
-#
-# 	Project:      VEXcode Project
-# 	Author:       VEX
-# 	Created:
-# 	Description:  VEXcode IQ Python Project
-#
-# ------------------------------------------
-
-# Library imports
 from vex import *
 
 # Constants
@@ -74,6 +17,8 @@ def move(amnt, speed):
     brain_inertial.set_rotation(0, DEGREES)
     left_drive_smart.set_position(0, DEGREES)
     right_drive_smart.set_position(0, DEGREES)
+    left_drive_smart.spin(FORWARD)
+    right_drive_smart.spin(FORWARD)
     while (right_drive_smart.position(DEGREES) + left_drive_smart.position(DEGREES)) / 2 < amnt:
         error = brain_inertial.rotation(DEGREES) * kp
         left_correct = speed - max(0, error)
