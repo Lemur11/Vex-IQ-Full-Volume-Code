21f37ae22441858ae9a3ce77b7f3e093ba8c7667
Simon LeMur <127620383+Lemur11@users.noreply.github.com>
2024-02-04 20:20:18 -0500

    Added backwards and made cleaner

--- comp3_auton.py
+++ comp3_auton.py
@@ -2,9 +2,8 @@ from vex import *
 
 # Constants
 kp = 1.5
-#if we multiply the degrees we wanna turn by this num, we get the amount we want our motors to spin
-# the full number i got was 2.29651852
-# but were using 2.3 cuz its good enough
+# if we multiply the degrees we wanna turn by this num, we get the amount we want our motors to spin
+# 2.3 is fine
 tcov = 2.3
 mommentum =  1
 # State thread
@@ -15,23 +14,28 @@ def state():
 
 s = Thread(state)
 
-
 def move(amnt, speed):
+    # speed decides if its forward or backward
     brain_inertial.set_rotation(0, DEGREES)
     left_drive_smart.set_position(0, DEGREES)
     right_drive_smart.set_position(0, DEGREES)
     left_drive_smart.spin(FORWARD)
     right_drive_smart.spin(FORWARD)
-    while (right_drive_smart.position(DEGREES) + left_drive_smart.position(DEGREES)) / 2 < amnt:
+    while (right_drive_smart.position(DEGREES) + left_drive_smart.position(DEGREES))/2 < amnt:
         error = brain_inertial.rotation(DEGREES) * kp
-        left_correct = speed - max(0, error)
-        right_correct = speed - abs(min(0, error))
+        if speed > 0:
+            left_correct = speed - max(0, error)
+            right_correct = speed - abs(min(0, error))
+        elif speed < 0:
+            left_correct = speed + abs(min(0, error))
+            right_correct = speed + max(0, error)
         left_drive_smart.set_velocity(left_correct)
         right_drive_smart.set_velocity(right_correct)
         wait(0.5, SECONDS)
     left_drive_smart.stop()
     right_drive_smart.stop()
-def trunr(degs):
+
+def turnr(degs):
     degs-= mommentum
     degs *= tcov
     while (abs(left_drive_smart.position(DEGREES))+abs(right_drive_smart.position(DEGREES)))/2 < degs:
@@ -41,7 +45,7 @@ def trunr(degs):
     right_drive_smart.stop()
     wait(0.5, SECONDS)
 
-def trunl(degs):
+def turnl(degs):
     degs -= mommentum
     degs *= tcov
     while (abs(left_drive_smart.position(DEGREES))+abs(right_drive_smart.position(DEGREES)))/2 < degs:
